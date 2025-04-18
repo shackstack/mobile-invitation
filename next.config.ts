0de225d918d54b452cae9aad383fbf00ab6aad62
Jeremy <102432453+shackstack@users.noreply.github.com>
2025-04-18 23:48:24 +0900

    Add remote image patterns to Next.js configuration: allow images from Unsplash, Placehold, and S3 for improved asset handling.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
+        protocol: "https",
+        hostname:
+          "wedding-invitation-static-source.s3.ap-northeast-2.amazonaws.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
